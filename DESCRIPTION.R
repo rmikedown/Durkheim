f7408853d3864fd02cb8af346ae3ef0e47417f14
rmikedown <31991599+rmikedown@users.noreply.github.com>
2017-11-29 18:01:50 -0500

    Add files via upload

---
+++ DESCRIPTION.R
@@ -0,0 +1,10 @@
+Package: Durkheim
+Title: CSVs from Suicide
+Version: 0.1
+Authors@R: person("Michael", "Smith", email = "smithmichaelpaul@gmail.com",
+                  role = c("aut", "cre"))
+Description: Lazy load all the data from the sociological classic
+Depends: R (>= 3.1.0)
+License: MIT
+LazyData: true
+URL: http://github.com/rmikedown/Durkheim
