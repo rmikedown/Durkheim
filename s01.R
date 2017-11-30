ddfaa72f90cdb69718fccd0de8f80cc839ee0171
Michael Smith <31991599+rmikedown@users.noreply.github.com>
2017-11-29 22:01:58 -0500

    Add files via upload

---
+++ s01.R
@@ -0,0 +1,14 @@
+#' Raw totals of suicides per year by country
+#'
+#' Data used in the introductory chapter of the book Suicide by Emile Durkheim
+#' Variables include Country, Year and Total.Suicides
+#' This is the first of fifty-six data sets in this package
+#'
+#' @format A data frame with 192 obs and 3 variables:
+#' \describe{
+#'   \item{Country}{Country, including France, Prussia and England}
+#'   \item{Year}{Year of the observation, numerical}
+#'
+#'@source \ Le Suicide: Étude de sociologie first published 1897, Paris
+
+"total"
